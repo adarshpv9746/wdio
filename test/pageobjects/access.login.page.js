dda458cf5bce87ac4113b7ac9761f616749f2ba2
Adarsh P V <adarshpv9@gmail.com>
2022-04-06 10:00:54 +0530

    ubuntu

--- test/pageobjects/access.login.page.js
+++ test/pageobjects/access.login.page.js
@@ -1,29 +1,26 @@
 ////////// LOGIN PAGE //////////
 
-const Page = require('./page');
-class LoginPage extends Page {
+class Accessportal {
     get inputdob () {return $(".mbsc-comp");}
     get setbutn () {return $(".mbsc-fr-btn1");}
     get inputlastdigit () {return $("[ng-reflect-id='phone0'] [placeholder]");}
     get checkbox () {return $(".mdl-checkbox__ripple-container");}
     get btnSubmit () {return $(".primary");}
+    get mbsc() { return $("div:nth-of-type(41)"); }
 
     async login () {
+        await this.inputlastdigit.setValue("1111");
         await this.checkbox.click();
         await this.inputdob.click();
+        await this.mbsc.click();
         await this.setbutn.click();
-        await browser.pause(1000)
-        await this.inputlastdigit.setValue("1111");
-        await browser.waitUntil(() => this.btnSubmit.isClickable())
+
         await this.btnSubmit.click();
-        await browser.pause(8000)
-    }
-    open () {
-        return super.open('login');
+        await browser.pause(3000)
     }
 }
 
-module.exports = new LoginPage();
+module.exports = new Accessportal();
 
 
 
